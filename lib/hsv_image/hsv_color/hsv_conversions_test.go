f20bfce29c2aedbda64f5956e967330824db17ee
Mitch Chapman <mchapman87501@gmail.com>
2021-03-21 17:34:06 -0600

    Restructuring.  WIP.

--- lib/hsv_image/hsv_color/hsv_conversions_test.go
+++ lib/hsv_image/hsv_color/hsv_conversions_test.go
@@ -1,4 +1,4 @@
-package color
+package hsv_color
 
 import (
 	"fmt"
--- lib/hsv_image/hsv_image.go
+++ lib/hsv_image/hsv_image.go
@@ -1,8 +1,10 @@
-package color
+package hsv_image
 
 import (
 	"image"
 	"image/color"
+
+	"com.dmoonc/mchapman87501/mars_2020_img_utils/lib/hsv_image/hsv_color"
 )
 
 type HSVImage struct {
@@ -12,32 +14,7 @@ type HSVImage struct {
 	Rect   image.Rectangle
 }
 
-// Create an HSVImage from an image.RGBA.
-// The image origin will be at 0, 0.
-func HSVImageFromRGBA(rgba *image.RGBA) HSVImage {
-	width := rgba.Rect.Dx()
-	height := rgba.Rect.Dy()
-	pixelSize := 3 // H,S,V
-	numPixels := width * height
-	rect := rgba.Rect
-	result := HSVImage{make([]float64, numPixels*pixelSize), width * pixelSize, rect}
-
-	srcOffset := 0
-	destOffset := 0
-	for y := rect.Min.Y; y < rect.Max.Y; y++ {
-		for x := rect.Min.X; x < rect.Max.X; x++ {
-			srcSlice := rgba.Pix[srcOffset : srcOffset+4]
-			destSlice := result.Pix[destOffset : destOffset+3]
-			destSlice[0], destSlice[1], destSlice[2] = RGB8ToHSV(srcSlice[0], srcSlice[1], srcSlice[2])
-
-			srcOffset += 4
-			destOffset += 3
-		}
-	}
-	return result
-}
-
-func (p *HSVImage) ColorModel() color.Model { return HSVModel }
+func (p *HSVImage) ColorModel() color.Model { return hsv_color.HSVModel }
 
 func (p *HSVImage) Bounds() image.Rectangle { return p.Rect }
 
@@ -45,16 +22,16 @@ func (p *HSVImage) At(x, y int) color.Color {
 	return p.HSVAt(x, y)
 }
 
-func (p *HSVImage) HSVAt(x, y int) HSV {
+func (p *HSVImage) HSVAt(x, y int) hsv_color.HSV {
 	if !(image.Point{x, y}.In(p.Rect)) {
-		return HSV{
+		return hsv_color.HSV{
 			H: 0,
 			S: 0,
 			V: 0,
 		}
 	}
 	i := p.PixOffset(x, y)
-	return HSV{p.Pix[i], p.Pix[i+1], p.Pix[i+2]}
+	return hsv_color.HSV{p.Pix[i], p.Pix[i+1], p.Pix[i+2]}
 }
 
 func (p *HSVImage) PixOffset(x, y int) int {
@@ -66,14 +43,14 @@ func (p *HSVImage) Set(x, y int, c color.Color) {
 		return
 	}
 	i := p.PixOffset(x, y)
-	c1 := HSVModel.Convert(c).(HSV)
+	c1 := hsv_color.HSVModel.Convert(c).(hsv_color.HSV)
 	s := p.Pix[i : i+3 : i+3]
 	s[0] = c1.H
 	s[1] = c1.S
 	s[2] = c1.V
 }
 
-func (p *HSVImage) SetHSVAt(x, y int, c HSV) {
+func (p *HSVImage) SetHSVAt(x, y int, c hsv_color.HSV) {
 	if !(image.Point{x, y}.In(p.Rect)) {
 		return
 	}
@@ -100,4 +77,29 @@ func (p *HSVImage) SubImage(rect image.Rectangle) *HSVImage {
 
 func (p *HSVImage) Opaque() bool {
 	return true
+}
+
+// Create an HSVImage from an image.RGBA.
+// The image origin will be at 0, 0.
+func HSVImageFromRGBA(rgba *image.RGBA) HSVImage {
+	width := rgba.Rect.Dx()
+	height := rgba.Rect.Dy()
+	pixelSize := 3 // H,S,V
+	numPixels := width * height
+	rect := rgba.Rect
+	result := HSVImage{make([]float64, numPixels*pixelSize), width * pixelSize, rect}
+
+	srcOffset := 0
+	destOffset := 0
+	for y := rect.Min.Y; y < rect.Max.Y; y++ {
+		for x := rect.Min.X; x < rect.Max.X; x++ {
+			srcSlice := rgba.Pix[srcOffset : srcOffset+4]
+			destSlice := result.Pix[destOffset : destOffset+3]
+			destSlice[0], destSlice[1], destSlice[2] = hsv_color.RGB8ToHSV(srcSlice[0], srcSlice[1], srcSlice[2])
+
+			srcOffset += 4
+			destOffset += 3
+		}
+	}
+	return result
 }
