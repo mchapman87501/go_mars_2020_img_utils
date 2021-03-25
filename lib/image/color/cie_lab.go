82e2825d9ce8f2506e8aa375c3f20e8a9f9f06c5
Mitch Chapman <mchapman87501@gmail.com>
2021-03-25 10:24:10 -0600

    Discrepancies appear < least-noticeable diff. Tested via the untested deltaE implementation :)

--- lib/image/color/cie_lab.go
+++ lib/image/color/cie_lab.go
@@ -1,7 +1,6 @@
 package color
 
 import (
-	"fmt"
 	"image/color"
 	"math"
 )
@@ -41,9 +40,7 @@ func cieLabToRGB(labL, laba, labb float64) (r, g, b uint32) {
 
 func rgbToCIELab(r, g, b uint32) (labL, laba, labb float64) {
 	x, y, z := rgbToCIEXYZ(r, g, b)
-	fmt.Printf("rgb(0x%x, 0x%x, 0x%x) -> XYZ(%v, %v, %v)\n", r, g, b, x, y, z)
 	labL, laba, labb = cieXYZToLabD65(x, y, z)
-	fmt.Printf("     -> Lab(%v, %v, %v)\n", labL, laba, labb)
 	return
 }
 
@@ -53,9 +50,9 @@ const cieDelta float64 = 6.0 / 29.0
 const cieD3 = cieDelta * cieDelta * cieDelta
 
 // Caveat: these match python colormath, rather than Wikipedia.
-const d65IllumX = 0.95047
+const d65IllumX = 0.950489
 const d65IllumY = 1.0
-const d65IllumZ = 1.08883
+const d65IllumZ = 1.08884
 
 func cieLabD65ToXYZ(labL, laba, labb float64) (x, y, z float64) {
 	ty := (labL + 16.0) / 116.0
@@ -72,13 +69,10 @@ func cieXYZToLabD65(x, y, z float64) (labL, laba, labb float64) {
 	xIllum := x / d65IllumX
 	yIllum := y / d65IllumY
 	zIllum := z / d65IllumZ
-	fmt.Printf("     -> xyzIllum(%v, %v, %v)\n", xIllum, yIllum, zIllum)
 
 	xScaled := scaleXYZToLabD65(xIllum)
 	yScaled := scaleXYZToLabD65(yIllum)
 	zScaled := scaleXYZToLabD65(zIllum)
-
-	fmt.Printf("     -> xyzScaled(%v, %v, %v)\n", xScaled, yScaled, zScaled)
 
 	labL = 116.0*yScaled - 16.0
 	laba = 500.0 * (xScaled - yScaled)
@@ -98,4 +92,102 @@ func scaleXYZToLabD65(component float64) float64 {
 		return math.Pow(component, 1.0/3.0)
 	}
 	return (7.787 * component) + (16.0 / 116.0)
+}
+
+// Get the CIE 2000 ΔE between 2 CIELab colors.
+// A return value < 2.3 indicates a human probably would
+// not notice any difference in the two colors.
+func DeltaECIE2000(lab1, lab2 CIELab) float64 {
+	return math.Sqrt(DeltaESqrCIE2000(lab1, lab2))
+}
+
+func DeltaESqrCIE2000(lab1, lab2 CIELab) float64 {
+	// I have no idea whether this is correct, or where it
+	// may fail with NaNs :)
+	avgLp := (lab1.L + lab2.L) / 2.0
+
+	b1Sqr := lab1.B * lab1.B
+	b2Sqr := lab2.B * lab2.B
+	c1 := math.Sqrt(lab1.A*lab1.A + b1Sqr)
+	c2 := math.Sqrt(lab2.A*lab2.A + b2Sqr)
+	avgC := (c1 + c2) / 2.0
+	avgC7 := math.Pow(avgC, 7.0)
+
+	pow25_7 := math.Pow(25.0, 7.0)
+
+	g := (1.0 - math.Sqrt(avgC7/(avgC7+pow25_7))) / 2.0
+
+	a1p := lab1.A * (1.0 + g)
+	a2p := lab2.A * (1.0 + g)
+
+	c1p := math.Sqrt(a1p*a1p + b1Sqr)
+	c2p := math.Sqrt(a2p*a2p + b2Sqr)
+	avgCp := (c1p + c2p) / 2.0
+
+	h1p := posRadToDeg(math.Atan2(lab1.B, a1p))
+	h2p := posRadToDeg(math.Atan2(lab2.B, a2p))
+	avgHp := (h1p + h2p) / 2.0
+	if math.Abs(h1p-h2p) > 180.0 {
+		avgHp += 180.0
+	}
+
+	t := (1.0 -
+		0.17*math.Cos(degToRad(avgHp-30.0)) +
+		0.24*math.Cos(degToRad(2.0*avgHp)) +
+		0.32*math.Cos(degToRad(3.0*avgHp+6.0)) -
+		0.2*math.Cos(degToRad(4.0*avgHp-63.0)))
+
+	deltaLp := lab2.L - lab1.L
+	deltaCp := c2p - c1p
+
+	deltaHp := h2p - h1p
+	if math.Abs(deltaHp) > 180.0 {
+		if h2p <= h1p {
+			deltaHp += 360.0
+		} else {
+			deltaHp -= 360.0
+		}
+	}
+	deltaHp = 2.0 * math.Sqrt(c1p*c2p) * math.Sin(degToRad(deltaHp)/2.0)
+
+	avgLPBias := avgLp - 50.0
+	aLPBSqr := avgLPBias * avgLPBias
+
+	sL := 1.0 + ((0.015 * aLPBSqr) / math.Sqrt(20.0+aLPBSqr))
+	sC := 1.0 + 0.045*avgCp
+	sH := 1.0 + 0.015*avgCp*t
+
+	aHp := (avgHp - 275.0) / 25.0
+	aHpSqr := aHp * aHp
+	deltaRo := 30.0 * math.Exp(-aHpSqr)
+
+	avgCp7 := math.Pow(avgCp, 7.0)
+
+	rC := 2.0 * math.Sqrt(avgCp7/(avgCp7+pow25_7))
+	rT := -rC * math.Sin(2.0*degToRad(deltaRo))
+
+	kL := 1.0
+	kC := 1.0
+	kH := 1.0
+
+	t0 := deltaLp / (sL * kL)
+	t1 := deltaCp / (sC * kC)
+	t2 := deltaHp / (sH * kH)
+	return (t0 * t0) + (t1 * t1) + (t2 * t2) + rT*t1*t2
+}
+
+func posRadToDeg(radians float64) float64 {
+	result := radToDeg(radians)
+	if result < 0.0 {
+		return result + 360.0
+	}
+	return result
+}
+
+func radToDeg(radians float64) float64 {
+	return 180.0 * radians / math.Pi
+}
+
+func degToRad(degrees float64) float64 {
+	return math.Pi * degrees / 180.0
 }
