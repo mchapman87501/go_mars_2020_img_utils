376e0f139c9364bc469c7cd52b89d411e0c6fd64
Mitch Chapman <mchapman87501@gmail.com>
2021-03-30 14:41:51 -0600

    Create the sandbox as needed.

--- build_all.sh
+++ build_all.sh
@@ -2,5 +2,8 @@
 
 # Build all of the commands and put them in a sandbox directory.
 # Avoids polluting ${GOPATH}/bin, eh.
+if [ ! -d sandbox ]; then
+    mkdir sandbox
+fi
 go build -o sandbox ./...
 
